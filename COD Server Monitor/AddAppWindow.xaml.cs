88b63203c37a117822468221cd0eab6a8e6e00a6
Alex Skiff <alex.skiff@scifusion360.com>
2020-01-08 12:24:23 -0500

    Add project files.

---
+++ COD Server Monitor/AddAppWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+
+namespace COD_Server_Monitor
+{
+   public partial class AddAppWindow : Window
+   {
+      public String FilePath;
+      public String Arguments;
+      public Boolean AutoRestart;
+      public Boolean AddApplication;
+
+      public AddAppWindow ()
+      {
+         InitializeComponent ();
+
+         AddApplication = false;
+      }
+
+      private void AddApplication_BrowseClick (object sender, RoutedEventArgs e)
+      {
+         OpenFileDialog dialog = new OpenFileDialog ();
+         dialog.Filter = "EXE Files|*.exe";
+         if (dialog.ShowDialog () == true)
+         {
+            this.FilePath = dialog.FileName;
+            this.FilePathTextBox.Text = this.FilePath;
+            this.AddButton.IsEnabled = true;
+         }
+      }
+
+      private void AddApplication_AddClick (object sender, RoutedEventArgs e)
+      {
+         this.Arguments = this.ArgumentsTextBox.Text;
+         this.AutoRestart = this.AutoRestartCheckBox.IsChecked == true;
+
+         if (this.Arguments.Equals ("Arguments"))
+            this.Arguments = String.Empty;
+
+         AddApplication = true;
+         this.Close ();
+      }
+
+      private void AddApplication_CancelClick (object sender, RoutedEventArgs e)
+      {
+         this.Close ();
+      }
+
+      private void AddApplication_ArgumentsEnterFocus (object sender, RoutedEventArgs e)
+      {
+         if (this.ArgumentsTextBox.Text.Equals ("Arguments"))
+            this.ArgumentsTextBox.Text = String.Empty;
+      }
+
+      private void AddApplication_ArgumentsLeaveFocus (object sender, RoutedEventArgs e)
+      {
+         if (this.ArgumentsTextBox.Text.Equals (String.Empty))
+            this.ArgumentsTextBox.Text = "Arguments";
+      }
+   }
+}
---
+++ COD Server Monitor/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace COD_Server_Monitor
+{
+   public partial class MainWindow : Window
+   {
+      private ObservableCollection<ApplicationRow> AppCollection;
+      private DispatcherTimer AppMonitor;
+
+      public MainWindow ()
+      {
+         InitializeComponent ();
+
+         AppMonitor = new DispatcherTimer ();
+         AppMonitor.Tick += new EventHandler (CheckApplicationStatus);
+         AppMonitor.Interval = new TimeSpan (0, 0, 5); //Every 5 seconds
+
+         AppCollection = new ObservableCollection<ApplicationRow> ();
+
+         ApplicationGrid.ItemsSource = AppCollection;
+      }
+
+      private void ApplicationGrid_ContextClick (object sender, RoutedEventArgs e)
+      {
+         AddAppWindow addApp = new AddAppWindow ();
+         addApp.ShowDialog ();
+
+         if (addApp.AddApplication)
+         {
+            String FileName = System.IO.Path.GetFileName (addApp.FilePath);
+
+            AppCollection.Add (new ApplicationRow ()
+            {
+               Name = FileName,
+               Path = addApp.FilePath,
+               Arguments = addApp.Arguments,
+               AutoRestart = addApp.AutoRestart
+            });
+         }
+      }
+
+      private void ApplicationGrid_StartClick (object sender, RoutedEventArgs e)
+      {
+         if (!AppMonitor.IsEnabled)
+            AppMonitor.Start ();
+      }
+
+      private void ApplicationGrid_RemoveClick (object sender, RoutedEventArgs e)
+      {
+
+      }
+
+      private void OutputText_ClearOutput (object sender, RoutedEventArgs e)
+      {
+         this.OutputText.Text = String.Empty;
+      }
+
+      private void CheckApplicationStatus (object sender, EventArgs e)
+      {
+
+      }
+   }
+
+   public class ApplicationRow
+   {
+      public String Name { get; set; }
+      public String Path { get; set; }
+      public String Arguments { get; set; }
+      public Boolean AutoRestart { get; set; }
+   }
+}
